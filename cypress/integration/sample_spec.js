0dbf6c2af3d7375e9cf60b79ef36ea5e2f68f499
ihysk <quest.for.curiosity1985@gmail.com>
2019-04-29 23:41:59 +0900

    Updated tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,10 +1,8 @@
 describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(false)
+  it('Does do much!', function() {
+    expect(true).to.equal(true)
   })
-})
 
-describe('My First Test', function() {
   it('Visits the Kitchen Sink', function() {
     cy.visit('https://example.cypress.io')
 
